12e661a7e364be86a5ac6481ccea5b6acba4253b
Chung Tran <bigearsenal@gmail.com>
2021-12-11 19:55:14 +0700

    fix: refactor, style

--- src/components/TKB.js
+++ src/components/TKB.js
@@ -1,4 +1,5 @@
 import {loadData} from "../api";
+import {createHeaderForWeek,createTimetableForClass} from "./table-generator";
 
 import workbook from "../../data/TKB.xlsx";
 
@@ -6,58 +7,6 @@ function sleep(ms) {
     return new Promise(
         resolve => setTimeout(resolve, ms)
     );
-}
-
-function createHeaderForWeek(tr) {
-    const headers = ["LOP","CA","TUAN","TIET","T2","T3","T4","T5","T6","T7"];
-    for (let i = 0; i < headers.length; i++) {
-        let th = document.createElement('th');
-        th.innerText = headers[i];
-        tr.appendChild(th);
-    }
-}
-
-function addTd(tr,text) {
-    let td = document.createElement('td');
-    td.innerText = text;
-    tr.appendChild(td);
-}
-
-function addTr(tbody,texts) {
-    let tr = document.createElement('tr');
-    for (let i = 0; i < texts.length; i++) {
-        addTd(tr,texts[i]);
-    }
-    tbody.appendChild(tr);
-}
-
-function fillTable(tbody, classes) {
-    for (let i = 0; i < classes.length; i++) {
-        addTr(tbody, [
-            classes[i].name,
-            classes[i].shift,
-            1,
-            classes[i].lessonIndexes[0]
-        ].concat([
-            classes[i].timetable.mon[classes[i].lessonIndexes[0]],
-            classes[i].timetable.tue[classes[i].lessonIndexes[0]],
-            classes[i].timetable.wed[classes[i].lessonIndexes[0]],
-            classes[i].timetable.thu[classes[i].lessonIndexes[0]],
-            classes[i].timetable.fri[classes[i].lessonIndexes[0]],
-            classes[i].timetable.sat[classes[i].lessonIndexes[0]],
-        ]))
-
-        for (let j = 0; j < 4; j++) {
-            addTr(tbody, ["", "", "", ""].concat([
-                classes[i].timetable.mon[classes[i].lessonIndexes[j]],
-                classes[i].timetable.tue[classes[i].lessonIndexes[j]],
-                classes[i].timetable.wed[classes[i].lessonIndexes[j]],
-                classes[i].timetable.thu[classes[i].lessonIndexes[j]],
-                classes[i].timetable.fri[classes[i].lessonIndexes[j]],
-                classes[i].timetable.sat[classes[i].lessonIndexes[j]]
-            ]));
-        }
-    }
 }
 
 async function TKB() {
@@ -91,6 +40,6 @@ async function TKB() {
 
     // read excel file
     let {classes,teachers} = loadData(workbook);
-    fillTable(tbody,classes)
+    createTimetableForClass(tbody,classes)
 }
 export default TKB;
---
+++ src/components/table-generator.js
@@ -0,0 +1,55 @@
+export function createHeaderForWeek(tr) {
+    const headers = ["LOP","CA","TUAN","TIET","T2","T3","T4","T5","T6","T7"];
+    for (let i = 0; i < headers.length; i++) {
+        let th = document.createElement('th');
+        th.innerText = headers[i];
+        tr.appendChild(th);
+        tr.className = "last";
+    }
+}
+
+export function addTd(tr,text) {
+    let td = document.createElement('td');
+    td.innerText = text;
+    tr.appendChild(td);
+}
+
+export function addTr(tbody,texts,isLast) {
+    let tr = document.createElement('tr');
+    if (isLast) {
+        tr.className = "last";
+    }
+    for (let i = 0; i < texts.length; i++) {
+        addTd(tr, texts[i]);
+    }
+    tbody.appendChild(tr);
+}
+
+export function createTimetableForClass(tbody, classes) {
+    for (let i = 0; i < classes.length; i++) {
+        addTr(tbody, [
+            classes[i].name,
+            classes[i].shift,
+            1,
+            classes[i].lessonIndexes[0] + 1
+        ].concat([
+            classes[i].timetable.mon[classes[i].lessonIndexes[0]],
+            classes[i].timetable.tue[classes[i].lessonIndexes[0]],
+            classes[i].timetable.wed[classes[i].lessonIndexes[0]],
+            classes[i].timetable.thu[classes[i].lessonIndexes[0]],
+            classes[i].timetable.fri[classes[i].lessonIndexes[0]],
+            classes[i].timetable.sat[classes[i].lessonIndexes[0]],
+        ]))
+
+        for (let j = 1; j < 4; j++) {
+            addTr(tbody, ["", "", "", classes[i].lessonIndexes[j] + 1].concat([
+                classes[i].timetable.mon[classes[i].lessonIndexes[j]],
+                classes[i].timetable.tue[classes[i].lessonIndexes[j]],
+                classes[i].timetable.wed[classes[i].lessonIndexes[j]],
+                classes[i].timetable.thu[classes[i].lessonIndexes[j]],
+                classes[i].timetable.fri[classes[i].lessonIndexes[j]],
+                classes[i].timetable.sat[classes[i].lessonIndexes[j]]
+            ]), j == 3);
+        }
+    }
+}
