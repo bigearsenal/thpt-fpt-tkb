9fd33dae15c6c1fa731f4d3bc22a194e02ec2b84
Chung Tran <bigearsenal@gmail.com>
2021-12-12 01:20:31 +0700

    fix: recursivelyFill

--- src/api/table-generator.js
+++ src/api/table-generator.js
@@ -1,4 +1,4 @@
-import {printClassesTimetable} from './table-html'
+import {printClassesTimetable} from './table-html';
 
 // MARK: - Private functions
 function sleep(ms) {
@@ -7,7 +7,64 @@ function sleep(ms) {
     );
 }
 
-function recursivelyFill(classes, lessonIndex, dayIndex) {
+async function recursivelyFill(thead, tbody, classes, row, column) {
+    // get needed variables
+    const dayIndex = column;
+    const classIndex = getClassIndex(row);
+    let lessonIndex = getClassLessonIndex(row);
+    if (classes[classIndex].shift === "CHIEU") {
+        lessonIndex = lessonIndex + 4;
+    }
+
+    // if classIndex === classes.count of class, we reached the end
+    if (classIndex === classes.length) {
+        return true
+    }
+
+    // TODO: find possible variants, sort if needed
+    let variants = [];
+
+    // for variant in variants
+    for (let i = 0; i < variants.count; i++) {
+        // fill classes timetable
+        classes[classIndex].timetable[dayIndex][lessonIndex] = variants[i];
+
+        // TODO: - Fill teacher's timetable
+
+        // print table
+        printClassesTimetable(thead, tbody, classes);
+        await sleep(300); // sleep to see result in real time
+
+        // next
+        let indexes = nextCellIndex(row, column);
+        let fillNextResult = await recursivelyFill(
+            thead,
+            tbody,
+            classes,
+            indexes.rowIndex,
+            indexes.columnIndex
+        );
+
+        // detect case
+        if (fillNextResult === true) {
+            // POSSIBLE CASE
+            return true
+        }
+    }
+
+    // IMPOSSIBLE CASE
+    // clear classes timetable
+    classes[classIndex].timetable[dayIndex][lessonIndex] = ""
+
+    // TODO: - Clear teacher timetable
+
+    // print
+    printClassesTimetable(thead, tbody, classes);
+    await sleep(300); // sleep to see result in real time
+
+    return false
+
+    // MARK: - algorithm 2
     // condition to end loop (false/true)
 
     // if there are possible variant, attempt, return these value, else find all possible variants (and sort), save variants, attempt = 0
@@ -21,50 +78,29 @@ function recursivelyFill(classes, lessonIndex, dayIndex) {
     // if fill next = true, return true
 
     // else save attempt + 1, fill current
-
-    if (fill(classes, lessonIndex, dayIndex) === true) {
-        const next = nextIndexes(lessonIndex, dayIndex);
-        if (next.lessonIndex > classes.length * 4) {
-            return true
-        }
-        return recursivelyFill(classes, next.lessonIndex, next.dayIndex, 0)
-    } else {
-        const previous = previousIndexes(lessonIndex, dayIndex);
-        if (previous.lessonIndex === -1) {
-            alert("Can not create scheduler");
-            return false
-        }
-        return recursivelyFill(classes, previous.lessonIndex, previous.dayIndex, attempt + 1)
-    }
 }
 
-function fill(classes, lessonIndex, dayIndex) {
-    return true;
+function getClassIndex(lessonIndex) {
+    return Math.floor(lessonIndex/4)
 }
 
-function previousIndexes(lessonIndex, dayIndex) {
-    if (dayIndex === 0) {
+function getClassLessonIndex(lessonIndex) {
+    return lessonIndex % 4
+}
+
+function nextCellIndex(rowIndex, columnIndex) {
+    if (columnIndex === 5) {
         return {
-            lessonIndex: lessonIndex - 1,
-            dayIndex: 5
+            rowIndex: rowIndex + 1,
+            columnIndex: 0
         }
     }
-    return {lessonIndex, dayIndex: dayIndex - 1}
-}
-
-function nextIndexes(lessonIndex, dayIndex) {
-    if (dayIndex === 5) {
-        return {
-            lessonIndex: lessonIndex + 1,
-            dayIndex: 0
-        }
-    }
-    return {lessonIndex, dayIndex: dayIndex + 1}
+    return {rowIndex: rowIndex, columnIndex: columnIndex + 1}
 }
 
 // MARK: - Public function
 export async function createTimetableForClass(thead, tbody, classes) {
-    // recursivelyFill(classes, 0, 0);
+    // await recursivelyFill(thead, tbody, classes, 0, 0);
 
     printClassesTimetable(thead, tbody, classes);
     await sleep(300);
