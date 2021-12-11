de7ed47fa425456286f390bc0428510539999fc6
Chung Tran <bigearsenal@gmail.com>
2021-12-11 10:57:23 +0700

    feat: add table template

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,19 @@
 import {Workbook} from "exceljs";
+// Private functions
+async function readExcelFile() {
+    const workbook = new Workbook();
+    await workbook.xlsx.readFile("../TKB.xlsx");
+    return workbook;
+}
 
+// Public functions
+export async function loadData() {
+    let workbook = await readExcelFile();
+
+}
 export async function getPost(id) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
     const json = await response.json();
 
     return json;
-}
-
-export async function readExcelFile() {
-    const workbook = new Workbook();
-    await workbook.xlsx.readFile("../TKB.xlsx");
-    return workbook;
 }
--- src/components/TKB.js
+++ src/components/TKB.js
@@ -1,3 +1,5 @@
+import {readExcelFile} from "../api";
+
 function sleep(ms) {
     return new Promise(
         resolve => setTimeout(resolve, ms)
@@ -5,16 +7,42 @@ function sleep(ms) {
 }
 
 async function TKB() {
+    // document div
     const div = document.createElement('div');
-    const h1 = document.createElement('h1');
-    
     div.className = 'main';
     document.body.appendChild(div);
+
+    // title label
+    const h1 = document.createElement('h1');
+    const h1Text = document.createTextNode('Thời khoá biểu');
+    h1.appendChild(h1Text);
     div.appendChild(h1);
 
+    // table
+    let border = '1px solid black'
+    const tbl = document.createElement('table');
+    tbl.style.border = border;
+    div.appendChild(tbl);
+    for (let i = 0; i < 3; i++) {
+        const tr = tbl.insertRow();
+        for (let j = 0; j < 2; j++) {
+            if (i === 2 && j === 1) {
+                break;
+            } else {
+                const td = tr.insertCell();
+                td.appendChild(document.createTextNode(`asdfasdfasdCell I${i}/J${j}`));
+                td.style.border = border;
+                if (i === 1 && j === 1) {
+                    td.setAttribute('rowSpan', '2');
+                }
+            }
+            await sleep(1000)
+        }
+    }
+
+
     for (let i = 0; i < 10; i++) {
-        const h1Text = document.createTextNode('Hll Webpack-Babel-Boilerplate!' + i);
-        h1.appendChild(h1Text);
+
         await sleep(1000)
     }
 }
